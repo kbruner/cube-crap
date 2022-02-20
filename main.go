79037726a15fadd6237411b788d6094ab8f588a6
Karen Bruner <kbruner@users.noreply.github.com>
2022-02-20 08:46:50 -0800

    Add slice moves to FRU

--- main.go
+++ main.go
@@ -1,68 +1,73 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "math/rand"
-    "strconv"
-    s "strings"
-    "time"
+	"flag"
+	"fmt"
+	"math/rand"
+	"strconv"
+	s "strings"
+	"time"
 )
 
 func main() {
-    fruPtr := flag.Bool("fruOnly", false, "Use FRU moves only")
+	fruPtr := flag.Bool("fruOnly", false, "Use FRU moves only")
 
-    var moveArg string
-    flag.StringVar(&moveArg, "moveRange", "20", "Number of moves: integer or range")
+	var moveArg string
+	flag.StringVar(&moveArg, "moveCount", "20", "Number of moves: integer")
 
-    flag.Parse()
+	flag.Parse()
 
-    var moveRange []int
-    if s.Contains(moveArg, "-") {
-        for _, val := range s.Split(moveArg, "-") {
-            i, _ := strconv.Atoi(val)
-            moveRange = append(moveRange, i)
-        }
-    } else {
-        i, _ := strconv.Atoi(moveArg)
-        moveRange = append(moveRange, i, i)
-    }
+	i, _ := strconv.Atoi(moveArg)
+	moveCount := i
 
-    scramble(moveRange, *fruPtr)
+	scramble(moveCount, *fruPtr)
+//    cubeState := initCube()
+//    fmt.Println(cubeState)
+//	cubePrint(cubeState)	
 }
 
-func scramble(moveRange []int, fruOnly bool) {
-    moves := []string{"F", "R", "U"}
-    modifiers := [3]string{"", "'", "2"}
+func scramble(moveCount int, fruOnly bool) {
+	moves := []string{"F", "R", "U"}
 
-    seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-    if fruOnly != true {
-        moves = append(moves, "B", "D", "L")
-    }
+	if fruOnly != true {
+		moves = append(moves, "B", "D", "L")
+	} else {
+		moves = append(moves, "f", "r", "u")
+	}
 
-    var moveCount int
-    if moveRange[0] == moveRange[1] {
-        moveCount = moveRange[0]
-    } else {
-        moveCount = seed.Intn(moveRange[1] - moveRange[0] + 1) + moveRange[0]
-    }
+	var algo []string
+	prevMove := ""
 
-    var algo []string
-    prevMove := ""
+	for i := 0; i < moveCount; i++ {
+		var nextMove string
+		for {
+			nextMove = moves[seed.Intn(len(moves))]
+			if nextMove != prevMove {
+				break
+			}
+		}
 
-    for i := 0; i < moveCount; i++ {
-        var nextMove string
-        for {
-            nextMove = moves[seed.Intn(len(moves))]
-            if nextMove != prevMove {
-                break
-            }
-        }
-        
-        algo = append(algo, s.Join([]string{nextMove, modifiers[seed.Intn(len(modifiers))]}, ""))
-        prevMove = nextMove
-    }
+		prevMove = nextMove
 
-    fmt.Println(algo)
+		var next []string
+		next = append([]string{nextMove})
+
+		ccw := seed.Intn(2)
+		if ccw == 1 {
+			next = append(next, "'")
+		}
+
+		if s.ToUpper(nextMove) == nextMove && ccw == 0 {
+			turnCount := seed.Intn(2)
+			if turnCount == 1 {
+				next = append(next, "2")
+			}
+		}
+
+        algo = append(algo, s.Join(next, ""))
+	}
+
+	fmt.Println(algo)
 }
